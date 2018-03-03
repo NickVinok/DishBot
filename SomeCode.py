5c9e6dd77df29b08ab0f9980fa55ab5a7d40194b
NickVinok <nickvinok@yandex.ru>
2018-03-03 20:36:21 +0300

    Started to work on the project btw

--- SomeCode.py
+++ SomeCode.py
@@ -1,11 +1,24 @@
 import telebot
 import config
+from telebot import types
 
 bot = telebot.TeleBot(token=config.TOKEN)
 
+"""
 @bot.message_handler(content_types=["text"])
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id,
+     "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
+"""
+@bot.message_handler(commands = ["start"])
+def greetings(message):
+    bot.send_message(message.chat.id, config.GREETING_MESSAGE)
+
+@bot.message_handler(commands=["help"])
+def instructions(message):
+    bot.send_message(message.chat.id, config.INSTRUCTION_MESSAGE)
 
 if __name__ == '__main__':
     bot.polling(none_stop = True)
--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-TOKEN = "530462217:AAHBi0FL5okptX1lfNlX1QLY4TflreZLA6w"
+TOKEN = "481048280:AAExzNU_CX99Wr9p4FA1XcRJWrSqPHkWsEI"
+GREETING_MESSAGE = """Приветствую, я - бот дишевод, был создан для помощи тебе в выборе блюда для готовки
+Чтобы начать нажмите /start"""
