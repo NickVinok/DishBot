29205b4e4927f92fdb459593b485ee863a59c458
nohoneyy <lu.roanna@gmail.com>
2018-03-03 16:31:45 +0300

    test

--- bot.py
+++ bot.py
@@ -2,3 +2,7 @@ import config
 import telebot
 
 bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): 
+    bot.send_message(message.chat.id, message.text)
