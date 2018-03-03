9f55eb318a8e2775864908b87b7f83ab1c9d9c81
nohoneyy <lu.roanna@gmail.com>
2018-03-03 23:54:49 +0300

    вау

--- parser.py
+++ parser.py
@@ -1,12 +1,33 @@
 import bs4
 import requests
+import re
+
 
 url = 'http://ligakulinarov.ru/recepty?sort=new&page='
 ending = "#recipeboard"
 last = 259
 
-for i in range(last):
+
+
+'''with open('recipes.csv', 'wt', encoding="utf-8") as f:
+    f.write('"title";"type";"calories";"difficulties";"method";"link"'+'\n')'''
+
+
+for i in range(1, last+1):
     new_url = url + str(i) + ending
     r = requests.get(new_url)
-    with open('test.html', 'w') as output_file:
-    output_file.write(r.text.encode('cp1251'))
+    text = r.text
+    soup = bs4.BeautifulSoup(text, 'html.parser')
+    lst = []
+    Lis = soup.findAll("div", {"class": "author-wrap"})
+    soup = bs4.BeautifulSoup(str(Lis), "html.parser")
+    for a in soup.find_all('a', href=True):
+        if a["href"].startswith("/recepty"):
+            lst.append(a["href"])
+    for tmp_url in lst:
+        r =requests.get('http://ligakulinarov.ru'+tmp_url)
+        soup = bs4.BeautifulSoup(r.text, "html.parser")
+        soup.find('h1')
+        #soup.find ('value oneline')
+
+#'/recepty/drojjevye-pirogi/krupy-muka/pletenka-s-makom-i-orehami-104528'
