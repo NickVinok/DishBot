07fe212e08113f63ac0508bebd95327553c3488b
nohoneyy <lu.roanna@gmail.com>
2018-03-03 16:59:31 +0300

    grdg

--- bot.py
+++ bot.py
@@ -3,6 +3,20 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+
+''''
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): 
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
+'''
+
+
+@bot.message_handler(content_types=["text"])
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
