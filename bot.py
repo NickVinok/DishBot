c82d78c914b1961559d103d75273fae472d49ee2
NickVinok <nickvinok@yandex.ru>
2018-03-04 13:42:58 +0300

    Made ui for bot

--- bot.py
+++ bot.py
@@ -2,17 +2,17 @@ import telebot
 import config
 import sqlite3
 from telebot import types
+from db_work import initiate_query
 
 bot = telebot.TeleBot(token=config.TOKEN)
 
-"""
-@bot.message_handler(content_types=["text"])
-def default_test(message):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(url_button)
-    bot.send_message(message.chat.id,
-     "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
-"""
+meal_time = ["Обед", "Завтрак", "Ужин", "Легко перекусить"]
+diet = ["Детям", "Вегетарианская", "Низкоуглеводная", "Низкокалорийная", "Обычное"]
+difficulty = ["Для новичка","Средней сложности","Сложное"]
+cooking_method = ["В духовке", "Жарим", "Консервируем", "Варим"]
+cooking_time = ["<45 мин", "45-120 мин", "120< мин"]
+listOfChoises = ["", "","", "", ""]
+
 @bot.message_handler(commands = ["start"])
 def greetings(message):
     bot.send_message(message.chat.id, config.GREETING_MESSAGE)
@@ -23,7 +23,43 @@ def instructions(message):
 
 @bot.message_handler(commands=["go"])
 def action(message):
-    bot.send_message(message.chat.id, "Здесь начнётся основная программа")
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add(*[types.KeyboardButton(meal) for meal in meal_time])
+    markup.add("Мне повезёт")
+    message = bot.send_message(message.chat.id,"Давай определимся с твоими предпочтениями на сегодня", reply_markup=markup)
+    bot.register_next_step_handler(message, test)
+
+def test(msg):
+    if msg.text in meal_time:
+        listOfChoises[0] = msg.text
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add(*[types.KeyboardButton(die) for die in diet])
+        msg = bot.send_message(msg.chat.id,"а?", reply_markup=markup)
+        bot.register_next_step_handler(msg, test)
+    elif msg.text in diet:
+        listOfChoises[1] = msg.text
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add(*[types.KeyboardButton(diff) for diff in difficulty])
+        msg = bot.send_message(msg.chat.id,"а?", reply_markup=markup)
+        bot.register_next_step_handler(msg, test)
+    elif msg.text in difficulty:
+        listOfChoises[2] =msg.text
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add(*[types.KeyboardButton(method) for method in cooking_method])
+        msg = bot.send_message(msg.chat.id,"а?", reply_markup=markup)
+        bot.register_next_step_handler(msg, test)
+    elif msg.text in cooking_method:
+        listOfChoises[3] = msg.text
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add(*[types.KeyboardButton(time) for time in cooking_time])
+        msg = bot.send_message(msg.chat.id,"а?", reply_markup=markup)
+        bot.register_next_step_handler(msg, test)
+    elif msg.text in cooking_time:
+        listOfChoises[4] = msg.text
+        initiate_query(listOfChoises)
+    elif msg.text == "Мне повезёт":
+        bot.send_message(msg.chat.id, "Счастливик")
+
 
 if __name__ == '__main__':
     bot.polling(none_stop = True)
--- db_work.py
+++ db_work.py
@@ -4,22 +4,24 @@ import pandas as pd
 import parserWeb
 import requests
 
-
-ps = parserWeb.Parser()
-df = ps.parsepy()
-df.set_index("Title")
-db = sqlite3.connect(r'C:\Users\Ric124\Desktop\Programming\Python\CookProj\db.db')
-
-df.to_csv(r'C:\Users\Ric124\Desktop\Programming\Python\CookProj\out.csv', encoding = 'cp1251')
-chunks = pd.read_csv(r'C:\Users\Ric124\Desktop\Programming\Python\CookProj\out.csv',
- encoding='cp1251',index_col="Title", chunksize=3)
-for chunk in chunks:
-    chunk.columns = [column.replace(' ','_') for column in chunk.columns]
-    chunk.to_sql('Recipes', db, if_exists='append')
+def initiate_query(listOfChoises):
+    db = sqlite3.connect(r'C:\Users\Ric124\Desktop\Programming\Python\CookProj\db.db')
+    for i in range(len(listOfChoises)):
+        if listOfChoises[i] == "":
+            listOfChoises[i] = "NULL"
+    print(listOfChoises)
+    curr = db.cursor()
 
 
-"""cur = connection.cursor()
-cur.execute('''DROP TABLE IF EXISTS Counts''')
-cur.execute('''
-CREATE TABLE Counts(id INTEGER, Title TEXT, CookTime TEXT, Difficulty TEXT,
-Method TEXT, Period TEXT, Calories Text, Link TEXT)''')"""
+def add_to_db():
+    ps = parserWeb.Parser()
+    df = ps.parsepy()
+    df.set_index("Title")
+    db = sqlite3.connect(r'C:\Users\Ric124\Desktop\Programming\Python\CookProj\db.db')
+
+    df.to_csv(r'C:\Users\Ric124\Desktop\Programming\Python\CookProj\out.csv', encoding = 'cp1251')
+    chunks = pd.read_csv(r'C:\Users\Ric124\Desktop\Programming\Python\CookProj\out.csv',
+     encoding='cp1251',index_col="Title", chunksize=3)
+    for chunk in chunks:
+        chunk.columns = [column.replace(' ','_') for column in chunk.columns]
+        chunk.to_sql('Recipes', db, if_exists='append')
